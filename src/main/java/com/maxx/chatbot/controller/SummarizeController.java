4cc481520f3f00ff04faa5c8c87eb253b44e5285
madebymaxx <nishantjindaliitd247@gmail.com>
2026-09-12 17:08:32 +0530

    Initial commit

---
+++ src/main/java/com/maxx/chatbot/controller/SummarizeController.java
@@ -0,0 +1,23 @@
+package com.maxx.chatbot.controller;
+
+import com.maxx.chatbot.service.SummarizeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SummarizeController {
+
+    private SummarizeService summarizeService;
+
+    public SummarizeController(SummarizeService summarizeService) {
+        this.summarizeService = summarizeService;
+    }
+
+    @PostMapping("/summarize")
+    public String summarize(@RequestBody String ticket){
+        return summarizeService.summerize(ticket);
+    }
+}
